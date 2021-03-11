8939bcba47dbcac43ffe0ee4c0a00ace41a29956
fmarcos <fmarcosdev@gmail.com>
2021-03-11 11:59:12 -0300

    .

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import config from './config.js'
 
-
-
-
 const app = express();
+
+
+app.get('/', function(req, res){
+  res.send("hola mundo")
+});
+
+
+
 
 
 app.listen(process.env.PORT || config.development.server.port, function (err){
 if (err){
-  console.log('error en el server' + err);
+  console.log('error' + err);
 }else{
  console.log("RUNNING===========")
     console.log("==================")
